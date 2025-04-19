ec40bca4446639541a71950a59172336ad2b2a2f
Jimin Park <pjm950@gmail.com>
2025-04-20 01:41:08 +0900

    Update app.tsx

--- src/component/app.tsx
+++ src/component/app.tsx
@@ -1,7 +1,11 @@
 export function App() {
-    return <p>Hello, world!</p>;
+    return <div>
+        <h1><a href="https://github.com/123jimin/template.typescript.webapp">template.typescript.webapp</a></h1>
+        <p>This is a template created by <a href="https://github.com/123jimin">@123jimin</a> for creating a simple web application, using TypeScript and Preact.</p>
+    </div>;
 }
 
 export function Root() {
+    // Add context providers here as needed.
     return <App />;
 }
